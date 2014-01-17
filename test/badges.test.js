83c9cb81fa1ae4933cb9bc3d2ee37de8d92e256a
Andrew Hayward <github@andrewhayward.net>
2014-01-17 16:30:05 +0000

    Updating badge tests

--- test/badges.test.js
+++ test/badges.test.js
@@ -1,6 +1,6 @@
 const async = require('async');
-const badge = require('./fixtures/badge');
 const client = require('./client');
+const fixtures = require('./fixtures/badges');
 const server = require('./server');
 const test = require('tap').test;
 
@@ -10,89 +10,74 @@ function end (test) {
   }
 }
 
-test('Getting badges', function (t) {
+test('Creating badges', function (t) {
   server
-    .getAndReturn('/badges', {badges: [badge]})
-    .getAndReturn('/badges/test-badge', {badge: badge})
-    .getAndReturn('/badges/badge-not-found', {}, 404);
+    .postAndReturn('/badges', fixtures.creationSuccessResponse)
+    .postAndReturn('/badges', fixtures.creationDuplicateResponse, 409)
+    .postAndReturn('/badges', fixtures.creationErrorResponse, 400);
 
   async.series([
     function (done) {
-      client.getBadges(function (err, badges) {
-        t.notOk(err, 'No error was raised');
-        t.same(badges.length, 1, 'Correct number of badges returned');
+      client.createBadge(fixtures.testBadge, function (err, status) {
+        t.notOk(err, 'No error was raised when creating badge');
+        t.same(status, 'created', 'Badge was successfully created');
         done();
       });
     },
     function (done) {
-      client.getBadge('test-badge', function (err, badge) {
-        t.notOk(err, 'No error was raised');
+      client.createBadge(fixtures.testBadge, function (err, status) {
+        // Should probably confirm the type of error here
+        t.ok(err, 'An error was raised when creating duplicate badge');
+        t.notOk(status, 'Nothing should be returned');
+        done();
+      });
+    },
+    function (done) {
+      client.createBadge(fixtures.badBadge, function (err, status) {
+        // Should probably confirm the type of error here
+        t.ok(err, 'An error was raised when creating bad badge');
+        t.notOk(status, 'Nothing should be returned');
+        done();
+      });
+    }
+  ], end(t));
+});
+
+test('Getting badges', function (t) {
+  server
+    .getAndReturn('/badges', fixtures.getResponse)
+    .getAndReturn('/badges?archived=any', fixtures.getAllResponse)
+    .getAndReturn('/badges/test-badge', fixtures.getSingleResponse)
+    .getAndReturn('/badges/missing-badge', fixtures.notFoundResponse, 404);
+
+  async.series([
+    function (done) {
+      client.getBadges(function (err, badges) {
+        t.notOk(err, 'No error was raised when getting active badges');
+        t.same(badges.length, 1, 'Correct number of active badges returned');
+        done();
+      });
+    },
+    function (done) {
+      client.getAllBadges(function (err, badges) {
+        t.notOk(err, 'No error was raised when getting all badges');
+        t.same(badges.length, 2, 'Correct number of badges returned');
+        done();
+      })
+    },
+    function (done) {
+      client.getBadge(fixtures.testBadge, function (err, badge) {
+        t.notOk(err, 'No error was raised when getting badge');
         t.ok(badge, 'A badge was returned');
         t.same(badge.slug, 'test-badge', 'Appropriate badge was returned');
         done();
       });
     },
     function (done) {
-      client.getBadge('badge-not-found', function (err, badge) {
-        t.ok(err, 'An error was raised'); // Should confirm the type of error here
+      client.getBadge(fixtures.missingBadge, function (err, badge) {
+        // Should probably confirm the type of error here
+        t.ok(err, 'An error was raised when getting non-existent badge');
         t.notOk(badge, 'No badge should be returned');
-        done();
-      });
-    }
-  ], end(t));
-});
-
-test('Deleting badges', function (t) {
-  server
-    .deleteAndReturn('/badges/test-badge', {status: 'deleted'})
-    .getAndReturn('/badges/test-badge', {}, 404);
-
-  async.series([
-    function (done) {
-      client.deleteBadge('test-badge', function (err, status) {
-        t.notOk(err, 'No error was raised');
-        t.same(status, 'deleted', 'Badge was successfully deleted');
-        done();
-      });
-    },
-    function (done) {
-      client.getBadge('test-badge', function (err, badge) {
-        t.ok(err, 'An error was raised'); // Should confirm the type of error here
-        t.notOk(badge, 'Badge should no longer exist');
-        done();
-      });
-    }
-  ], end(t));
-});
-
-test('Creating badges', function (t) {
-  server
-    .postAndReturn('/badges', {status: 'created'})
-    .getAndReturn('/badges/test-badge', {badge: badge})
-    .postAndReturn('/badges',
-      {errors: [{name:'ValidatorError', message:'Missing value', field:"slug"}]}, 400);
-
-  async.series([
-    function (done) {
-      client.createBadge(badge, function (err, status) {
-        t.notOk(err, 'No error was raised');
-        t.same(status, 'created', 'Badge was successfully created');
-        done();
-      });
-    },
-    function (done) {
-      client.getBadge('test-badge', function (err, badge) {
-        t.notOk(err, 'No error was raised');
-        t.ok(badge, 'A badge was returned');
-        t.same(badge.slug, 'test-badge', 'New badge returned');
-        done();
-      });
-    },
-    function (done) {
-      var badBadge = {};
-      client.createBadge(badBadge, function (err, status) {
-        t.ok(err, 'An error was raised'); // Should confirm the type of error here
-        t.notOk(status, 'Nothing should be returned');
         done();
       });
     }
@@ -101,22 +86,54 @@ test('Creating badges', function (t) {
 
 test('Updating badges', function (t) {
   server
-    .putAndReturn('/badges/test-badge', {status: 'updated'})
-    .putAndReturn('/badges/test-badge',
-      {errors: [{name:"ValidatorError", message:"String is not in range", field:"title"}]}, 400);
+    .putAndReturn('/badges/test-badge', fixtures.updateSuccessResponse)
+    .putAndReturn('/badges/test-badge', fixtures.updateErrorResponse, 400)
+    .putAndReturn('/badges/missing-badge', fixtures.notFoundResponse, 404);
 
   async.series([
     function (done) {
-      client.updateBadge(badge, function (err, status) {
-        t.notOk(err, 'No error was raised');
+      client.updateBadge(fixtures.testBadge, function (err, status) {
+        t.notOk(err, 'No error was raised when updating badge');
         t.same(status, 'updated', 'Badge was successfully updated');
         done();
       })
     },
     function (done) {
-      var badBadge = {slug: 'test-badge', 'title': ''};
-      client.updateBadge(badBadge, function (err, status) {
-        t.ok(err, 'An error was raised');
+      client.updateBadge(fixtures.badBadge, function (err, status) {
+        // Should probably confirm the type of error here
+        t.ok(err, 'An error was raised when updating badge invalidly');
+        t.notOk(status, 'Nothing should be returned');
+        done();
+      });
+    },
+    function (done) {
+      client.updateBadge(fixtures.missingBadge, function (err, status) {
+        // Should probably confirm the type of error here
+        t.ok(err, 'An error was raised when updating badge invalidly');
+        t.notOk(status, 'Nothing should be returned');
+        done();
+      });
+    }
+  ], end(t));
+});
+
+test('Deleting badges', function (t) {
+  server
+    .deleteAndReturn('/badges/test-badge', fixtures.deleteSuccessResponse)
+    .deleteAndReturn('/badges/missing-badge', fixtures.notFoundResponse, 404);
+
+  async.series([
+    function (done) {
+      client.deleteBadge(fixtures.testBadge, function (err, status) {
+        t.notOk(err, 'No error was raised when deleting badge');
+        t.same(status, 'deleted', 'Badge was successfully deleted');
+        done();
+      });
+    },
+    function (done) {
+      client.deleteBadge(fixtures.missingBadge, function (err, status) {
+        // Should probably confirm the type of error here
+        t.ok(err, 'An error was raised when deleting a non-existent badge');
         t.notOk(status, 'Nothing should be returned');
         done();
       });
