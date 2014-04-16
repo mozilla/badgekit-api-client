3df3f5a9fbc3c0835c5f5833e376955adb1764f8
Erik Christensen <christensenep@gmail.com>
2014-04-16 14:04:54 -0700

    Adding ability to look up badge by claim code, and consume claim code when creating badge instance

--- models/instance.js
+++ models/instance.js
@@ -11,6 +11,24 @@ function Instance (data, parent) {
 
 Object.defineProperty(Instance, 'pathIdentifier', {value: 'email'});
 
-utils.initModel(Instance, '/instances', {}, BaseModel);
+utils.initModel(Instance, '/instances', {
+  create: function create (code, callback) {
+    var filter = this.constructor.name;
+    filter = filter.charAt(0).toLowerCase() + filter.substr(1);
+
+    var data = this;
+    data.claimCode = code;
+    
+    const options = {
+      path: this._parent._path + this.constructor.pathPart,
+      data: data,
+      filter: filter,
+      generator: new utils.Generator(this.constructor, this._parent)
+    }
+
+    this._remote.post(options, callback);
+  }
+}, BaseModel);
+
 
 exports = module.exports = Instance;
